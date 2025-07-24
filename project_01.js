b99e34b92b4c5a5e2944289ab3feefbc277bf81c
YunHwaYoung <ghkdud832@naver.com>
2025-07-24 21:29:32 +0900

    add: project_01.js 파일추가

---
+++ project_01.js
@@ -0,0 +1,14 @@
+
+
+
+/*
+ var bannerOffset = $('.backGround01' ).offset();
+
+$( window ).scroll( function(){
+ if ( $( document ).scrollTop() > bannerOffset.top ){
+ $('.backGround01' ).addClass( 'backGround01Fixed' );
+ console.log("안녕하세요");
+  }else { $('.backGround01' ).removeClass( 'backGround01Fixed' );
+  }
+  });
+*/
