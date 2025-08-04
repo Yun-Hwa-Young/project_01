56a2f5248dc3a3cabea6c57d6a9c1e5b00be5c7a
YunHwaYoung <ghkdud832@naver.com>
2025-08-04 15:26:07 +0900

    Feat add index.html

---
+++ docs/project_01.js
@@ -0,0 +1,302 @@
+
+
+import {videoData} from './video_data.js';
+import {subVideoData} from './subVideo_data.js';
+
+
+const content01 = document.querySelector(".content01");
+const contentButton01 = document.querySelector(".contentButton01");
+const container = document.getElementById('play_01');
+const blurContent = document.getElementById('blurContent');
+
+function timeAgo(dateString) {
+  const now = new Date();
+   const date = new Date(dateString);
+
+
+   const KST_OFFSET = 9 * 60 * 60 * 1000;
+   const nowKST = new Date(now.getTime() + KST_OFFSET);
+   const dateKST = new Date(date.getTime() + KST_OFFSET);
+
+   let diff = nowKST - dateKST;
+
+
+   if (diff < 0) return "방금 전";
+
+   const seconds = Math.floor(diff / 1000);
+   const minutes = Math.floor(diff / (1000 * 60));
+   const hours = Math.floor(diff / (1000 * 60 * 60));
+   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+   const weeks = Math.floor(days / 7);
+   const months = Math.floor(days / 30);
+   const years = Math.floor(days / 365);
+
+   if (seconds < 60) return `${seconds}초 전`;
+   if (minutes < 60) return `${minutes}분 전`;
+   if (hours < 24) return `${hours}시간 전`;
+   if (days < 7) return `${days}일 전`;
+   if (weeks < 5) return `${weeks}주 전`;
+   if (months < 12) return `${months}개월 전`;
+   return `${years}년 전`;
+}
+
+
+/*메인 화면의 동영상 리스트*/
+
+document.addEventListener('DOMContentLoaded', () => {
+
+if (!container) {
+    console.warn(' 메인페이지가 아닙니다. project_01.js 스크립트를 종료합니다.');
+    return;
+  }
+
+if (!container) {
+    console.error('play_01 요소를 찾을 수 없습니다. 여기는 리스트페이지');
+    return;
+  }
+
+videoData.forEach(video => {
+const timeText = timeAgo(video.upDate);
+const div = document.createElement('div');
+div.className = 'video-item';
+div.innerHTML = `
+<div class="col">
+        <div class="card">
+          <div class="cardAni01">
+            <a href="#" class="videoLink" data-video-id="${video.videoId}" data-image-src="${video.thumbnail}"><img src="${video.thumbnail}" class="card-img-top" alt="..."></a>
+          </div>
+          <div class="contentHead"><button class="contentButton"><i class="fa-brands fa-youtube" style="color: #ffffff;"></i> </button></div>
+          <div class="card-body01">
+            <p class="card-text02" >${video.title}</p>
+          </div>
+          <div class="card-body02">
+            <div class="card-text01">
+                <a style="font-weight: bold;">채널명 · </a>${video.channel}
+              </div>
+              <div class="card-text01">
+                조회수 ${video.views}회 · ${new Date(video.upDate).toISOString().split("T")[0]}일 · ${timeText}
+              </div>
+          </div>
+        </div>
+      </div>
+
+
+`;
+container.appendChild(div);
+
+
+});
+
+const links= document.querySelectorAll(".videoLink");
+
+links.forEach(link => {
+const videoId = link.dataset.videoId;
+const imageSrc = link.dataset.imageSrc;
+const parts = imageSrc.split('/');
+const middleValue = parts[parts.length -2 ];
+
+link.href  = `video.html?videoId=${encodeURIComponent(videoId)}&imageKey=${encodeURIComponent(middleValue)}`;
+});
+});
+
+
+
+
+
+
+
+
+
+/*구독버튼 누를때*/
+
+const setting03_3 = document.getElementById("setting03_3");
+if(setting03_3){
+
+document.getElementById("setting03_3").addEventListener("click", () => {
+
+const oldHeaderWrapper = document.querySelector(".headerWrapper");
+const oldSubContent = document.querySelector(".subContent");
+
+if (oldHeaderWrapper) oldHeaderWrapper.remove();
+if (oldSubContent) oldSubContent.remove();
+
+
+container.style.display ='none';
+contentButton01.style.display ='none';
+blurContent.style.display ='none';
+
+
+
+const headerWrapper = document.createElement('div');
+headerWrapper.classList.add('headerWrapper');
+ headerWrapper.style.display = 'flex';
+ headerWrapper.style.justifyContent = 'space-between'; // 왼쪽과 오른쪽 끝으로 배치
+ headerWrapper.style.alignItems = 'center';
+ headerWrapper.style.marginBottom = '30px';
+
+
+
+const div2 = document.createElement('div');
+div2.classList.add('adminBox');
+div2.style.display = 'flex';
+div2.style.gap = '10px';
+
+
+const p = document.createElement('p');
+p.className = 'video-item_p';
+p.textContent = "최신순";
+p.style.color='#F1F1F1';
+p.style.fontSize='20px';
+p.style.fontWeight='bold';
+p.style.margin = '0';
+
+const adminButton = document.createElement('button');
+const boardButton = document.createElement('button');
+const listButton = document.createElement('button');
+
+adminButton.textContent = '관리';
+adminButton.id='adminButton';
+boardButton.classList.add('fa-solid', 'fa-border-all', 'fa-lg');
+boardButton.style.color='#ffffff';
+boardButton.style.background = 'none';
+boardButton.style.border = 'none';
+boardButton.style.cursor = 'pointer';
+
+listButton.classList.add('fa-solid', 'fa-list-ul', 'fa-lg');
+listButton.style.color='#ffffff';
+listButton.style.background = 'none';
+listButton.style.border = 'none';
+listButton.style.cursor = 'pointer';
+
+
+div2.appendChild(adminButton);
+div2.appendChild(boardButton);
+div2.appendChild(listButton);
+
+div2.style.display = 'flex';
+div2.style.justifyContent = 'space-between';
+div2.style.alignItems = 'center';
+
+headerWrapper.appendChild(p);
+headerWrapper.appendChild(div2);
+
+
+content01.appendChild(headerWrapper);
+
+const div1 = document.createElement('div');
+div1.classList.add('subContent', 'row', 'row-cols-1','row-cols-md-3');
+
+
+
+subVideoData.forEach(video => {
+const div = document.createElement('div');
+div.className = 'video-item';
+div.innerHTML = `
+<div class="col">
+        <div class="card">
+          <div class="cardAni01">
+            <a href="#" class="videoLink" data-video-id="${video.videoId}" data-image-src="${video.thumbnail}"><img src="${video.thumbnail}" class="card-img-top" alt="..."></a>
+          </div>
+          <div class="contentHead"><button class="contentButton"> <i class="fa-brands fa-youtube" style="color: #ffffff;"></i></button></div>
+          <div class="card-body01">
+            <p class="card-text02" >${video.title}</p>
+          </div>
+          <div class="card-body02">
+            <div class="card-text01">
+                <a style="font-weight: bold;">채널명 · </a>${video.channel}
+              </div>
+              <div class="card-text01">
+                조회수 ${video.views}회 · ${new Date(video.upDate).toISOString().split("T")[0]}일
+              </div>
+          </div>
+        </div>
+      </div>
+
+`;
+div1.appendChild(div);
+ window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+content01.appendChild(div1);
+
+
+/*파라미터 보내기*/
+const links= document.querySelectorAll(".videoLink");
+
+links.forEach(link => {
+const videoId = link.dataset.videoId;
+const imageSrc = link.dataset.imageSrc;
+const parts = imageSrc.split('/');
+const middleValue = parts[parts.length -2 ];
+
+link.href  = `video.html?videoId=${encodeURIComponent(videoId)}&imageKey=${encodeURIComponent(middleValue)}`;
+});
+
+
+
+});
+}else{
+}
+
+
+
+
+/* 홈버튼 누를 때*/
+const setting03_1 = document.getElementById("setting03_1");
+
+if(setting03_1){
+document.getElementById("setting03_1").addEventListener("click", () => {
+const subContent = document.querySelector(".subContent");
+const headerWrapper = document.querySelector(".headerWrapper");
+const blurContent = document.getElementById('blurContent');
+
+  if (subContent) {
+     subContent.remove();
+   }
+
+   if (headerWrapper) {
+     headerWrapper.remove();
+   }
+
+   container.style.removeProperty('display');
+   contentButton01.style.removeProperty('display');
+   blurContent.style.removeProperty('display');
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+
+});
+}else{}
+
+
+
+const logo = document.getElementById("logo");
+
+if(setting03_1){
+
+
+logo.addEventListener("click", () => {
+
+const subContent = document.querySelector(".subContent");
+const headerWrapper = document.querySelector(".headerWrapper");
+
+  if (subContent) {
+     subContent.remove();
+   }
+
+   if (headerWrapper) {
+     headerWrapper.remove();
+   }
+
+
+ container.style.removeProperty('display');
+ contentButton01.style.removeProperty('display');
+
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+
+});
+}else{}
+
+
+
+
